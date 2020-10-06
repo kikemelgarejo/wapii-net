806bf43107824a9af52d1e7d042a3b2c1fb0c8d4
kikemelga <k.melgarejo123@gmail.com>
2020-10-05 21:12:56 -0300

    creacion ruta, vista y controlador para trends

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index]
   resources :posts, only: [:index]
+  resources :trends, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
