bf1b913e07ad98af760bc246fdcb0421045bfd09
kikemelga <k.melgarejo123@gmail.com>
2020-10-05 22:02:21 -0300

    configuracion root, y se añade navbar al layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :posts, only: [:index]
   resources :trends, only: [:index]
   resources :influencers, only: [:index]
+
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
