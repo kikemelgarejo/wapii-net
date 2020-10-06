d6e9160acae7e853188d9d19d7d42f54275a6fd7
kikemelga <k.melgarejo123@gmail.com>
2020-10-05 21:17:51 -0300

    por controlador de trends, se filtran datos extraidos de la API segun se requieren y se prueba en vista

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,2 +1,33 @@
 class TrendsController < ApplicationController
+    def index
+        @postordenados = []
+
+
+        posts = HTTParty.get('https://jsonplaceholder.typicode.com/posts')
+
+
+        posts.each do |post|
+
+        idpost = post["id"].to_s
+            
+        hash = Hash.new
+        user = HTTParty.get('https://jsonplaceholder.typicode.com/users/'+ post["userId"].to_s)
+        comentariosposts = HTTParty.get("https://jsonplaceholder.typicode.com/posts/"+ idpost +"/comments")
+
+        ncomtarios = 0 
+        comentariosposts.each do |count|
+            ncomtarios += 1
+        end
+     
+        
+        iduser = user["id"].to_s
+        
+        hash["name"] = user["name"]
+        hash["title"] = post["title"]
+        hash["body"] = post["body"]
+        hash["total"] = ncomtarios.to_s
+
+        @postordenados.push(hash)
+        end
+    end
 end
